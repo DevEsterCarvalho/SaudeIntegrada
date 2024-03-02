3da773b83fd7cb9bb23cbc181a8590df5b900142
DevEsterCarvalho <estercarvalho205@gmail.com>
2024-03-02 14:26:23 -0300

    editando tela de login

--- app/src/main/java/br/com/fiap/saudeintegrada/ui/theme/Type.kt
+++ app/src/main/java/br/com/fiap/saudeintegrada/ui/theme/Type.kt
@@ -2,9 +2,22 @@ package br.com.fiap.saudeintegrada.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import br.com.fiap.saudeintegrada.R
+
+
+val InterBlack = FontFamily(Font(R.font.inter_black))
+val InterBold = FontFamily(Font(R.font.inter_bold))
+val InterExtraBold = FontFamily(Font(R.font.inter_extra_bold))
+val InterExtraLight = FontFamily(Font(R.font.inter_extra_light))
+val InterLight = FontFamily(Font(R.font.inter_light))
+val InterMedium = FontFamily(Font(R.font.inter_medium))
+val InterRegular = FontFamily(Font(R.font.inter_regular))
+val InterSemiBold= FontFamily(Font(R.font.inter_semi_bold))
+val InterThin = FontFamily(Font(R.font.inter_thin))
 
 // Set of Material typography styles to start with
 val Typography = Typography(
