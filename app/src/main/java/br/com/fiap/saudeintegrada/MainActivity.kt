64b200d3907cede5067bb66c924838ebb13010f0
DevEsterCarvalho <estercarvalho205@gmail.com>
2024-03-02 11:32:19 -0300

    adicionando telas

--- app/src/main/java/br/com/fiap/saudeintegrada/MainActivity.kt
+++ app/src/main/java/br/com/fiap/saudeintegrada/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import br.com.fiap.saudeintegrada.screens.Login
 import br.com.fiap.saudeintegrada.ui.theme.SaudeIntegradaTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +18,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SaudeIntegradaTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
+
+
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SaudeIntegradaTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/br/com/fiap/saudeintegrada/screens/Login.kt
@@ -0,0 +1,19 @@
+package br.com.fiap.saudeintegrada.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.fiap.saudeintegrada.ui.theme.SaudeIntegradaTheme
+
+@Composable
+fun Login (modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPreview() {
+    SaudeIntegradaTheme {
+        Login()
+    }
+}
