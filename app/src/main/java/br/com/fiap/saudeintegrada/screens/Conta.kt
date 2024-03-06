f040b6335cd8a3895008e94ce7f0c1795b224c2b
DevEsterCarvalho <estercarvalho205@gmail.com>
2024-03-06 11:28:04 -0300

    finalizando nav

--- app/src/main/java/br/com/fiap/saudeintegrada/screens/Conta.kt
+++ app/src/main/java/br/com/fiap/saudeintegrada/screens/Conta.kt
@@ -2,18 +2,24 @@ package br.com.fiap.saudeintegrada.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.sharp.List
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.rounded.AccountCircle
 import androidx.compose.material.icons.rounded.CheckCircle
-import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
@@ -22,6 +28,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,64 +37,101 @@ import br.com.fiap.saudeintegrada.R
 
 @Composable
 fun Conta () {
-    Column (
-        verticalArrangement = Arrangement.SpaceBetween,
+    Box(
         modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xffFFFFFF)),
-    )
-    {
+        .fillMaxSize()
+        .background(Color(0xffFFFFFF)),
+        contentAlignment = Alignment.Center
+    ){
         Column (
+            verticalArrangement = Arrangement.SpaceBetween,
             modifier = Modifier
-                .align(Alignment.CenterHorizontally),
+                .fillMaxSize()
+                .background(Color(0xffFFFFFF)),
         )
         {
-            Text(text = "SAÚDE INTEGRADA" )
-            Text(text = "TERCEIRA IDADE")
+            Column (
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally),
+            )
+            {
+                Text(text = "SAÚDE INTEGRADA" )
+                Text(text = "TERCEIRA IDADE")
+            }
+            Card (
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally),
+            )
+            {
+                Image(painter = painterResource(id = R.drawable.icone_saude)
+                    , contentDescription = "ícone do projeto saúde integrada", modifier = Modifier.size(width = 49.dp, height = 49.dp) )
+            }
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Text(text = "Preencha seus dados para criar a conta")
+            }
+            Column (
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                OutlinedTextField(value = "", onValueChange = {})
+                OutlinedTextField(value = "", onValueChange = {})
+                OutlinedTextField(value = "", onValueChange = {})
+                OutlinedTextField(value = "", onValueChange = {})
+                OutlinedTextField(value = "", onValueChange = {})
+            }
+            Column {
+                Text(text = "Eu concordo com os termos e condições")
+                Text(text = "Termos e Condições")
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Enviar")
+                }
+            }
         }
-        Card (
+        Box (
             modifier = Modifier
-                .align(Alignment.CenterHorizontally),
-        )
-        {
-            Image(painter = painterResource(id = R.drawable.icone_saude)
-                , contentDescription = "ícone do projeto saúde integrada", modifier = Modifier.size(width = 49.dp, height = 49.dp) )
-        }
-        Row (
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            Text(text = "Preencha seus dados para criar a conta")
-        }
-        Column (
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            OutlinedTextField(value = "", onValueChange = {})
-            OutlinedTextField(value = "", onValueChange = {})
-            OutlinedTextField(value = "", onValueChange = {})
-            OutlinedTextField(value = "", onValueChange = {})
-            OutlinedTextField(value = "", onValueChange = {})
-        }
-        Column {
-            Text(text = "Eu concordo com os termos e condições")
-            Text(text = "Termos e Condições")
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "Enviar")
-            }
-        }
-        Row {
-            Button(onClick = { /*TODO*/ }) {
-                Icon(imageVector = Icons.Rounded.Home, contentDescription = "ícone home nav")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Icon(imageVector = Icons.Rounded.AccountCircle, contentDescription = "ícone usuário nav")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Icon(imageVector = Icons.Rounded.CheckCircle, contentDescription = "ícone verificado nav")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Icon(imageVector = Icons.AutoMirrored.Sharp.List, contentDescription = "ícone lista")
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 15.dp)
+                .padding(top = 16.dp)
+                .background(Color.White, shape = RoundedCornerShape(8.dp))
+                .border(1.dp, Color(0xffF2F2F7), shape = RoundedCornerShape(8.dp)),
+        ){
+            Row (
+                modifier = Modifier
+                    .padding(10.dp)
+            ){
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(Color(0xff5861E2)),
+                    shape = RoundedCornerShape(6.dp),
+                    contentPadding = PaddingValues(5.dp),
+                    modifier = Modifier
+                        .size(48.dp)
+                        .shadow(10.dp, shape = RoundedCornerShape(4.dp), clip = true)
+
+                ) {
+                    Icon(imageVector = Icons.Default.Home, contentDescription = "ícone home nav")
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(Color.White),
+                ) {
+                    Icon(imageVector = Icons.Rounded.AccountCircle, contentDescription = "ícone usuário nav", tint = Color.LightGray)
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(Color.White),
+                ) {
+                    Icon(imageVector = Icons.Rounded.CheckCircle, contentDescription = "ícone verificado nav", tint = Color.LightGray)
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(Color.White),
+                ) {
+                    Icon(imageVector = Icons.AutoMirrored.Sharp.List, contentDescription = "ícone lista", tint = Color.LightGray)
+                }
             }
         }
     }
--- app/src/main/java/br/com/fiap/saudeintegrada/screens/Login.kt
+++ app/src/main/java/br/com/fiap/saudeintegrada/screens/Login.kt
@@ -1,16 +1,21 @@
 package br.com.fiap.saudeintegrada.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.sharp.List
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.rounded.AccountCircle
 import androidx.compose.material.icons.rounded.CheckCircle
-import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
@@ -19,6 +24,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,88 +37,120 @@ import androidx.compose.material3.Button as Button
 
 @Composable
 fun Login () {
-    Column (
-        verticalArrangement = Arrangement.SpaceBetween,
+    Box(
         modifier = Modifier
-                .fillMaxSize()
-                .background(Color(0xffFFFFFF)),
+            .fillMaxSize()
+            .background(Color(0xffFFFFFF)),
+        contentAlignment = Alignment.Center
     ) {
         Column (
             modifier = Modifier
-                .align(Alignment.CenterHorizontally),
-        )
-        {
-            Text(text = "SAÚDE INTEGRADA" )
-            Text(text = "TERCEIRA IDADE")
-        }
-        Card (
+                .align(Alignment.Center)
+        ) {
+            Column (
                 modifier = Modifier
                     .align(Alignment.CenterHorizontally),
             )
-        {
-            Image(painter = painterResource(id = R.drawable.icone_saude)
-            , contentDescription = "ícone do projeto saúde integrada", modifier = Modifier.size(width = 49.dp, height = 49.dp) )
-        }
-        Column (
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            OutlinedTextField(value= "", onValueChange = {})
-            Text (text= "Esqueceu seu E-mail?")
-            OutlinedTextField(value = "", onValueChange = {})
-            Text (text= "Esqueceu sua senha?")
-            Button(onClick = {}){
-                Text(text= "Entre")
+            {
+                Text(text = "SAÚDE INTEGRADA" )
+                Text(text = "TERCEIRA IDADE")
             }
-        }
-        Row (modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            Text(text= "Ou entre com")
-        }
-        Row {
-            Button(onClick = {}) {
-                Image(painter = painterResource(id = R.drawable.google),
-                    contentDescription = "ícone google", modifier= Modifier.size(width = 26.dp, height = 26.5.dp))
-                Text(text= "Google")
+            Card (
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally),
+            )
+            {
+                Image(painter = painterResource(id = R.drawable.icone_saude)
+                    , contentDescription = "ícone do projeto saúde integrada", modifier = Modifier.size(width = 49.dp, height = 49.dp) )
             }
-            Button(onClick = {}) {
+            Column (
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                OutlinedTextField(value= "", onValueChange = {})
+                Text (text= "Esqueceu seu E-mail?")
+                OutlinedTextField(value = "", onValueChange = {})
+                Text (text= "Esqueceu sua senha?")
+                Button(onClick = {}){
+                    Text(text= "Entre")
+                }
+            }
+            Row (modifier = Modifier
+                .fillMaxWidth()
+            ) {
+                Text(text= "Ou entre com")
+            }
+            Row {
+                Button(onClick = {}) {
+                    Image(painter = painterResource(id = R.drawable.google),
+                        contentDescription = "ícone google", modifier= Modifier.size(width = 26.dp, height = 26.5.dp))
+                    Text(text= "Google")
+                }
+                Button(onClick = {}) {
 //                Icon(imageVector = Icons.Default.Facebook, contentDescription = "ícone facebook")
-                Image(painter = painterResource(id = R.drawable.facebook),
-                    contentDescription = "ícone facebook", modifier= Modifier.size(width = 26.dp, height = 26.5.dp))
-                Text(text= "Facebook")
+                    Image(painter = painterResource(id = R.drawable.facebook),
+                        contentDescription = "ícone facebook", modifier= Modifier.size(width = 26.dp, height = 26.5.dp))
+                    Text(text= "Facebook")
+                }
+            }
+            Column (modifier = Modifier
+                .fillMaxWidth()
+            ) {
+                Row {
+                    Text(text = "Não tem uma conta?")
+                    Text(text = "Registre")
+                }
+                Row {
+                    Text(text = "Precisa de ajuda?")
+                    Text(text = "Centro de Ajuda")
+                }
             }
         }
-        Column (modifier = Modifier
-            .fillMaxWidth()
-        ) {
-            Row {
-                Text(text = "Não tem uma conta?")
-                Text(text = "Registre")
-            }
-            Row {
-                Text(text = "Precisa de ajuda?")
-                Text(text = "Centro de Ajuda")
-            }
+        Box (
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 15.dp)
+                .padding(top = 16.dp)
+                .background(Color.White, shape = RoundedCornerShape(8.dp))
+                .border(1.dp, Color(0xffF2F2F7), shape = RoundedCornerShape(8.dp)),
+        ){
+            Row (
+                modifier = Modifier
+                    .padding(10.dp)
+            ){
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(Color(0xff5861E2)),
+                    shape = RoundedCornerShape(6.dp),
+                    contentPadding = PaddingValues(5.dp),
+                    modifier = Modifier .size(48.dp)
+                        .shadow(10.dp, shape = RoundedCornerShape(4.dp), clip = true)
 
-        }
-        Row {
-            Button(onClick = { /*TODO*/ }) {
-                Icon(imageVector = Icons.Rounded.Home, contentDescription = "ícone home nav")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Icon(imageVector = Icons.Rounded.AccountCircle, contentDescription = "ícone usuário nav")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Icon(imageVector = Icons.Rounded.CheckCircle, contentDescription = "ícone verificado nav")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Icon(imageVector = Icons.AutoMirrored.Sharp.List, contentDescription = "ícone lista")
+                ) {
+                    Icon(imageVector = Icons.Default.Home, contentDescription = "ícone home nav")
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(Color.White),
+                ) {
+                    Icon(imageVector = Icons.Rounded.AccountCircle, contentDescription = "ícone usuário nav", tint = Color.LightGray)
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(Color.White),
+                ) {
+                    Icon(imageVector = Icons.Rounded.CheckCircle, contentDescription = "ícone verificado nav", tint = Color.LightGray)
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(Color.White),
+                ) {
+                    Icon(imageVector = Icons.AutoMirrored.Sharp.List, contentDescription = "ícone lista", tint = Color.LightGray)
+                }
             }
         }
     }
 }
-
 
 
 @Preview(showSystemUi = true)
